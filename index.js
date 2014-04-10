324f6d5f8f93daebe25cec5e6c14e949ba217261
Eddie Antonio Santos <easantos@ualberta.ca>
2014-04-10 13:48:00 -0600

    Dropped need to use #create(); version bump.

--- index.js
+++ index.js
@@ -2,15 +2,29 @@
  * Creates a new fluent Harrison object.
  */
 
-var Harrison = require('./lib/harrison');
+var methods = require('./lib/harrison');
 
 module.exports = makeHarrison;
 
 function makeHarrison(app, options) {
+  var subapp;
+
   if (!app) {
     throw new Error('Must take Express app as first argument.');
   }
 
-  return new Harrison(app, options);
-}
+  options = options || {};
 
+  /* Looks and acts just like middleware! */
+  subapp = function (req, res, next) {
+    return subapp.middleware(req, res, next);
+  };
+
+  methods.init.call(subapp, app, options);
+
+  /* Add just one method. */
+  subapp.addApp = methods.addApp.bind(subapp);
+  subapp.create = methods.create.bind(subapp);
+
+  return subapp;
+}
